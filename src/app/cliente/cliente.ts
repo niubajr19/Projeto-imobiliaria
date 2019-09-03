a40bb426c4a7ede059b0f0ed245c5e2ea885a160
enosteteo <enosfrancisco@gmail.com>
2019-09-03 14:38:33 -0300

    T7.2 - Classe Cliente -

---
+++ src/app/cliente/cliente.ts
@@ -0,0 +1,13 @@
+export class Cliente {
+    constructor(
+        public nomeCompleto: String,
+        public cpf: String,
+        public dataNascimento: String,
+        public profissao: String,
+        public rg: String,
+        public endereco: String,
+        public email: String,
+        public telefone: number, 
+        ) { }        
+}
+
