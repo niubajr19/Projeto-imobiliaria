deb489df37625fe3161a84976f2d0371c6042162
niubajr19 <lucas.antonio199@outlook.com>
2019-09-02 21:02:09 -0300

    Adição home/listClient Component

---
+++ src/app/list-client/list-client.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list-client.component.html',
+  styleUrls: ['./list-client.component.css']
+})
+export class ListClientComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
