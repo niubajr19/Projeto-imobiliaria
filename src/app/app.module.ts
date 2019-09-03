12a6a0ed53d9bb72d6509812129ca62bada16d95
enosteteo <enosfrancisco@gmail.com>
2019-09-03 16:30:56 -0300

    mov..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ClienteComponent } from './cliente/cliente.component';
-import { ListClientComponent } from './list-client/list-client.component';
+import { ListClientComponent } from './client/list-client/list-client.component';
 import { MenuComponent } from './menu/menu.component';
 import { ClientComponent } from './client/client.component';
+import { ClientRegisterComponent } from './client/client-register/client-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ClienteComponent,
     ListClientComponent,
     MenuComponent,
-    ClientComponent
+    ClientComponent,
+    ClientRegisterComponent
   ],
   imports: [
     BrowserModule,
