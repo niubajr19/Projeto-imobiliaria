661f4b9a8d25b329db7f0dfe64d9ef594aefe082
enosteteo <enosfrancisco@gmail.com>
2019-09-03 15:23:02 -0300

    Correção cliente.component.ts - removeu linhas desnec

--- src/app/cliente/cliente.component.ts
+++ src/app/cliente/cliente.component.ts
@@ -7,17 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteComponent implements OnInit {
 
-  constructor(
-    public nomeCompleto: String,
-    public cpf: String,
-    public dataNascimento: String,
-    public profissao: String,
-    public rg: String,
-    public endereco: String,
-    public email: String,
-    public telefone: number, 
-    ) { }
-
+  constructor() { }
   ngOnInit() {
   }
 
