31411664cc8c6fab1ba3065782a233741449c5f0
eduardopessoa89 <eduardopessoa89@gmail.com>
2019-09-03 22:49:20 -0300

    Adiçao de Clientes na memoria

--- src/app/client/client.service.ts
+++ src/app/client/client.service.ts
@@ -9,7 +9,10 @@ export class ClientService {
 
   lastId = 0;
 
-  clients: Client[];
+  clients: Client[] = [
+    { name: 'Junior', dateOfBirth: '04/05/1999', cpf: '12345678901', profession: 'Contador', rg: '5437623', adress: 'Rua Qualquer', email: 'Junior...', telephone: 111111111 ,id: ++this.lastId},
+    { name: 'João', dateOfBirth: '20/07/1997', cpf: '12354321123', profession: 'Estudante', rg: '3214323', adress: 'Rua Logo ali', email: 'João...', telephone: 333333333 ,id: ++this.lastId},
+    { name: 'Maria', dateOfBirth: '05/01/2000', cpf: '12345678906', profession: 'Analista de Sistemas', rg: '1232122', adress: 'Rua Nova', email: 'Maria...', telephone: 222222222 ,id: ++this.lastId},  ];
 
   constructor() { }
 
