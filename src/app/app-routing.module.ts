7b41a0dc179fc310b5fefc28bbeee144e7e84c1e
niubajr19 <lucas.antonio199@outlook.com>
2019-09-10 18:26:26 -0300

    Estrutura do Forms criada - HTML/CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { ClientComponent } from './client/client.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListClientComponent } from './client/list-client/list-client.component';
+import { ClientRegisterComponent } from './client/client-register/client-register.component';
+
 
 
 
 const routes: Routes = [
   {path:'client', component: ClientComponent},
-  {path:'client/list', component: ListClientComponent}
+  {path:'client/list', component: ListClientComponent},
+  {path:'client/register',component:ClientRegisterComponent}
 
 ];
 
